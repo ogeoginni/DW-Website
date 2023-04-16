c3eac676ce846d0797e2f0e6cfa1cec73e4c1847
ogeoginni <92025350+ogeoginni@users.noreply.github.com>
2023-04-16 17:23:51 +0100

    updated content in about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,27 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>Dawnwatch is a one-stop shop for all project management needs.</h1>
+      <p>
+        At Dawnwatch, we are committed to bringing unequalled professionalism
+        and delivering superior services to the satisfaction of our clients.
+      </p>
+      <br />
+      <p>
+        As a global engineering, procurement construction and project management
+        company providing infrastructure solutions, we protect our reputation
+        for cost-effective project delivery.
+      </p>
+      <br />
+      <p>
+        With many years of experience delivering complex projects in challenging
+        environments, our team approach is centred on cultivation the
+        participation of key stakeholeders.
+      </p>
+      <br />
+      <p>
+        We ensure successful project delivery by meeting pre-planned schedules,
+        budgets and specifications.
+      </p>
     </div>
   );
 };
