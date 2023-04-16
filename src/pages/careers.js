41764a2d2bb001b5c41e98c1e98d69e10e2d3d51
ogeoginni <92025350+ogeoginni@users.noreply.github.com>
2023-04-16 21:11:02 +0100

    updated css

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -3,7 +3,10 @@ import React from "react";
 const Careers = () => {
   return (
     <div>
-      <h1>Join our team of professionals</h1>
+      <h1>
+        We are always looking for exceptional talent to join our team of
+        professionals
+      </h1>
     </div>
   );
 };
