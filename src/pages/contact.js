9c1c483b17e79ab49906f70f69b4d1f12cab08be
ogeoginni <92025350+ogeoginni@users.noreply.github.com>
2023-04-16 20:27:31 +0100

    updated css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,42 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Mail us on info@dawnwatchgroup.com</h1>
+    <div className="content-container container">
+      <div className="content">
+        <h1 className="mb-5 mt-5">📮 Let's get in touch!</h1>
+        <h3 className="mt-4">Email</h3>
+        <p>
+          <a
+            href="mailto:info@dawnwatchgroup.com"
+            rel="noreferrer"
+            title="Send us an email"
+          >
+            Send an email
+          </a>
+        </p>
+        <h3>Twitter</h3>
+        <p>
+          <a
+            href="https://www.twitter.com/dawnwatch"
+            rel="noreferrer"
+            title="Go to our twitter page"
+            target="_blank"
+          >
+            Check out our twitter handle
+          </a>
+        </p>
+        <h3>Instagram</h3>
+        <p>
+          <a
+            href="https://www.instagram.com/dawnwatch"
+            rel="noreferrer"
+            title="View our Instagram page"
+            target="_blank"
+          >
+            Find us on Instagram
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
